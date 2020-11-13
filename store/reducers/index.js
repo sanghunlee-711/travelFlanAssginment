4855701b48aaf1bb69756cfaf5533c803df67350
cloudlee <cloudlee@MacBook-Pro-9.local>
2020-11-13 15:17:02 +0900

    Updates

--- store/reducers/index.js
+++
@@ -1,5 +0,0 @@
-// root reducer를 만들기 위한 combineReducer
-import { combineReducers } from "redux";
-import uploadReducer from "./uploadReducer";
-
-export default combineReducers({ uploadReducer });
--- sum.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require("./sum");
-
-test("1더하기 2는 3", () => {
-  expect(sum(1, 2)).toEqual(3);
-});
